74ddad3e1553463e2a99b08ca87c39753bc122ed
Rakib Hussain <156150712+iamrakibhussain@users.noreply.github.com>
2026-06-21 11:21:26 +0600

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,8 @@ import authRouter from './routes/auth.routes.js'
 
 const app = express();
 app.use(cors({
-  // origin: process.env.FRONTEND_URL,
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
+  // origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 }))
 app.use(cookieParser());
